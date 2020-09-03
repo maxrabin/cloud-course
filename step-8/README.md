cb67346c4e691dfe8dab8d95df7fb6a393748428
Max Rabin <rabin.max@gmail.com>
2020-09-04 00:22:45 +0300

    step 8

---
+++ step-8/README.md
@@ -0,0 +1,3 @@
+Step 8
+======
+Create a Security Group to allow inbound traffic and run a web server
---
+++ step-8/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+WEBSITE_DOMAIN_NAME=`jq -r .WebsiteDomainName < ../config.json`
+STACK_NAME=`jq -r .CloudformationStackName < ../config.json`
+
+echo Cloudformation Deploy...
+aws cloudformation deploy \
+    --template-file ./template.yaml \
+    --capabilities CAPABILITY_IAM \
+    --parameter-overrides WebsiteDomainName=$WEBSITE_DOMAIN_NAME \
+    --stack-name $STACK_NAME
+
+echo Sync Website Files...
+aws s3 sync frontend s3://$WEBSITE_DOMAIN_NAME/
+
+echo Outputs...
+aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs' --output yaml
